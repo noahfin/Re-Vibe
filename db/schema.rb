4ece26b2019f77aa3da2f4ade916713bf2a567ef
Noah Finnerman <nonothetoad@gmail.com>
2017-01-07 15:12:51 -0500

    Add relationship between users and goals

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107001202) do
+ActiveRecord::Schema.define(version: 20170107200653) do
 
   create_table "goals", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_goals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
