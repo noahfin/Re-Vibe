7ac2473d97b383cf7225f0f6240d0eb22bd3206d
Noah Finnerman <nonothetoad@gmail.com>
2017-01-07 13:32:13 -0500

    Add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170106232059) do
+ActiveRecord::Schema.define(version: 20170107001202) do
+
+  create_table "goals", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
